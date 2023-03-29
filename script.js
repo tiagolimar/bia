24b039c0629dd8951d66c5f467eed0f38260c60c
tiagolimar <tiagolima.r2@gmail.com>
2023-03-29 07:44:54 -0300

    conclusão do código sem teste

--- script.js
+++ script.js
@@ -4,12 +4,20 @@ const palpites = document.querySelector('#display-palpites');
 const palpite = document.querySelector('#palpite');
 const imagem = document.querySelector('#imagem');
 
-
 let numero_premiado = 0;
 let tentativas_restantes = 0;
 let valor_palpite = '';
 let resposta_validada = false;
 let lista_de_palpites = [];
+let reiniciar = false;
+
+function derrota(){
+    imagem.src = '03.png';
+    status_.innerHTML = 'Você perdeu, o número era: ';
+    status_valor.innerHTML = numero_premiado;
+    alert('Você perdeu! Pressione Enter para reiniciar o jogo.')
+    reiniciar = true;
+}
 
 function validar_palpites(num){
     resposta_validada = false;
@@ -38,16 +46,18 @@ function atualizar_num_palpites(num){
 function calcular_resultado(num){
     if (num < numero_premiado){
         alert('O seu palpite está abaixo.');
+        if(tentativas_restantes == 0){derrota()}
     }else if (num > numero_premiado){
         alert('O seu palpite está acima.');
+        if(tentativas_restantes == 0){derrota()}
     }else{
+        imagem.src = 'img/02.png';
         status_.innerHTML = "Parabéns o número é: ";
         status_valor.innerHTML = numero_premiado;
         alert('Você ganhou! Pressione Enter para reiniciar o jogo.')
+        reiniciar = true;
     }
-    // TEM QUE TESTAR NO 'ATUALIZAR NUM PALPITES' SE É A ÚLTIMA TENTATIVA
 }
-
 
 function palpitar(){
     valor_palpite = palpite.value;
@@ -58,6 +68,7 @@ function palpitar(){
 }
 
 function inicializar(){
+    reiniciar = false;
     imagem.src = 'img/01.png';
     numero_premiado = Math.floor(Math.random() * 100) + 1;
     tentativas_restantes = 10;
@@ -70,6 +81,10 @@ inicializar();
 
 palpite.addEventListener('keydown',function(tecla){
     if(tecla.key === "Enter"){
-        palpitar();
+        if(reiniciar){
+            inicializar();
+        }else{
+            palpitar();
+        }
     }
 });
